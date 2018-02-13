e0f4a83b6cd6c716fa9ed28e9a157e02b4c40bce
Wes <wes.r.hawkins@gmail.com>
2018-02-12 16:02:37 -0800

    Multiplayer boilerplate

---
+++ FlowProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField] private float orbitDist = 50f;
+	[SerializeField] private float orbitSpeed = 2f;
+
+
+	void Update () {
+		transform.rotation *= Quaternion.AngleAxis(Time.deltaTime * orbitSpeed, Vector3.up);
+		transform.position = -transform.forward * orbitDist;
+	} // End of Update().
+
+} // End of CameraController.
