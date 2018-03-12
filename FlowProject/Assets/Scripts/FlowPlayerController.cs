2cc4f0f06ece1a8cc9667cb1e9e9041a73bc7994
Wes <wes.r.hawkins@gmail.com>
2018-03-12 15:14:09 -0700

    manual control of anchor point using WASDRF.

--- FlowProject/Assets/Scripts/FlowPlayerController.cs
+++ FlowProject/Assets/Scripts/FlowPlayerController.cs
@@ -18,6 +18,26 @@ public class FlowPlayerController : NetworkBehaviour{
 
         transform.position = CameraController.Inst.transform.position;
         transform.rotation = CameraController.Inst.transform.rotation;
+
+
+		// Manual origin movement
+		Vector3 throttle = Vector3.zero;
+		if(Input.GetKey(KeyCode.W))
+			throttle.z += 1f;
+		if(Input.GetKey(KeyCode.S))
+			throttle.z -= 1f;
+		if(Input.GetKey(KeyCode.D))
+			throttle.x += 1f;
+		if(Input.GetKey(KeyCode.A))
+			throttle.x -= 1f;
+		if(Input.GetKey(KeyCode.R))
+			throttle.y += 1f;
+		if(Input.GetKey(KeyCode.F))
+			throttle.y -= 1f;
+
+
+		CameraController.Inst.transform.parent.position += transform.rotation * throttle * Time.deltaTime;
+
     } // End of Update().
 
 } // End of FlowPlayerController.
