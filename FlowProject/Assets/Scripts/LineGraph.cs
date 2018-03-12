32fe70e6e874d8bc384a2123a9461a945181abed
Wes <wes.r.hawkins@gmail.com>
2018-03-12 16:11:56 -0700

    Line graphs!

---
+++ FlowProject/Assets/Scripts/LineGraph.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGraph : MonoBehaviour {
+
+
+	private LineRenderer myLineRenderer = null;
+	private List<float> dataPoints = new List<float>();
+	[SerializeField] private int numDataPoints = 100;
+
+	[SerializeField] private float takeDataRate = 10f;
+	private float takeDataCooldown = 0f;
+
+	public float sample = 0f;
+
+
+	// Use this for initialization
+	void Awake () {
+		myLineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		takeDataCooldown -= Time.deltaTime;
+		while(takeDataCooldown <= 0f){
+			takeDataCooldown += (1f / takeDataRate);
+
+			dataPoints.Add(sample);
+
+			if(dataPoints.Count > numDataPoints)
+				dataPoints.RemoveAt(0);
+		}
+
+		Vector3[] graphPositions = new Vector3[dataPoints.Count];
+		for(int i = 0; i < dataPoints.Count; i++){
+			graphPositions[i] = transform.position + (transform.rotation * (Vector3.right * i) + (Vector3.up * dataPoints[i] * 10f));
+		}
+
+		myLineRenderer.positionCount = graphPositions.Length;
+		myLineRenderer.SetPositions(graphPositions);
+		
+	}
+}
