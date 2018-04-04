7eb1c9ef58ed210aa37272651b92c5e5f29a90b9
Wes <wes.r.hawkins@gmail.com>
2018-04-04 15:01:46 -0700

    Further graph work, synchronized 'mellow' value from EEG, rigged up to visualization.

--- FlowProject/Assets/Scripts/FlowPlayerController.cs
+++ FlowProject/Assets/Scripts/FlowPlayerController.cs
@@ -7,16 +7,28 @@ public class FlowPlayerController : NetworkBehaviour{
 	public static FlowPlayerController local = null; public static FlowPlayerController Local { get { return local; } }
 	[SerializeField] private FlowtrailController trailController = null;
 
-	[SyncVar] private float eegVal = 0f;
-	private float lastEegVal = 0f;
+	private float updateCooldown = 0f;
+
+	// Synchronized Variables ============================================== //
+	[SyncVar] private float eegConcent = 0f;
+	private float eegConcent_last = 0f;
+
+	[SyncVar] private float eegMellow = 0f;
+	private float eegMellow_last = 0f;
+
+	[SyncVar] private float hrv = 0f;
+	private float hrv_last = 0f;
+
 	[SyncVar] private Color playerColor = Color.white;
 
-	private float updateCooldown = 0f;
+
 
 
 	public override void OnStartLocalPlayer(){
 		local = this;
-		GameObject.Find("button_eeg").GetComponent<Button>().onClick.AddListener(ToggleVal);
+		GameObject.Find("slider_eeg_concent").GetComponentInChildren<Slider>().onValueChanged.AddListener(Cmd_SetEEGConcent);
+		GameObject.Find("slider_eeg_mellow").GetComponentInChildren<Slider>().onValueChanged.AddListener(SetEEGMellow);
+		GameObject.Find("slider_hrv").GetComponentInChildren<Slider>().onValueChanged.AddListener(SetHRV);
 
 		GameObject.Find("button_color_blue").GetComponent<Button>().onClick.AddListener(() => Cmd_SetColor(Color.blue));
 		GameObject.Find("button_color_red").GetComponent<Button>().onClick.AddListener(() => Cmd_SetColor(Color.red));
@@ -33,9 +45,11 @@ public class FlowPlayerController : NetworkBehaviour{
 	} // End of OnStartServer().
 
 
-	void Update(){
+	private void Update(){
 
-		trailController.SetIntensity(eegVal);
+		trailController.SetCount(eegConcent);
+		trailController.SetSpeed(eegMellow);
+		trailController.SetWidth(eegMellow);
 		trailController.SetColor(playerColor);
 
         if (isLocalPlayer){
@@ -65,11 +79,24 @@ public class FlowPlayerController : NetworkBehaviour{
 				updateCooldown = 0.5f;
 
 				if(MuseManager.Inst.MuseDetected){
-					eegVal = MuseManager.Inst.LastConcentrationMeasure;
-					if(eegVal != lastEegVal){
-						lastEegVal = eegVal;
-						Cmd_SetEEGVal(eegVal);
+					eegConcent = MuseManager.Inst.LastConcentrationMeasure;
+					GameObject.Find("slider_eeg_concent").GetComponentInChildren<Slider>().value = eegConcent;
+					if(eegConcent != eegConcent_last){
+						eegConcent_last = eegConcent;
+						Cmd_SetEEGConcent(eegConcent);
 					}
+
+					eegMellow = MuseManager.Inst.LastMellowMeasure;
+					GameObject.Find("slider_eeg_mellow").GetComponentInChildren<Slider>().value = eegMellow;
+					if(eegMellow != eegMellow_last){
+						eegMellow_last = eegMellow;
+						Cmd_SetEEGMellow(eegMellow);
+					}
+				}
+
+				if(hrv != hrv_last){
+					hrv_last = hrv;
+					Cmd_SetHRV(hrv);
 				}
 			}
 			
@@ -82,16 +109,35 @@ public class FlowPlayerController : NetworkBehaviour{
 
 	// Networking ============================================================ //
 
-	// EEG Val --------------------------------------------------------------- //
-	public void ToggleVal(){
-		eegVal = 1 - eegVal;
-		Cmd_SetEEGVal(eegVal);
+	public void SetEEGConcent(float newVal){
+		eegConcent = newVal;
+		Cmd_SetEEGConcent(eegConcent);
+	} // End of SetEEGConcent().
+	
+	[Command] private void Cmd_SetEEGConcent(float newVal){
+		eegConcent = newVal;
+	} // End of Cmd_SetEEGConcent().
+
+
+	public void SetEEGMellow(float newVal){
+		eegMellow = newVal;
+		Cmd_SetEEGMellow(eegMellow);
 	} // End of ToggleVal().
 	
-	[Command] private void Cmd_SetEEGVal(float newEEGVal){
-		eegVal = newEEGVal;
-	} // End of UpdateEEGVal().
+	[Command] private void Cmd_SetEEGMellow(float newVal){
+		eegMellow = newVal;
+	} // End of Cmd_SetEEGMellow().
 
+
+	public void SetHRV(float newVal){
+		hrv = newVal;
+		Cmd_SetHRV(hrv);
+	} // End of ToggleVal().
+	
+	[Command] private void Cmd_SetHRV(float newVal){
+		hrv = newVal;
+	} // End of Cmd_SetEEGMellow().
+	
 
 
 	// Player Color ---------------------------------------------------------- //
